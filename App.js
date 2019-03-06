487965f558f9fd30496e0929284c9e629fddb66f
Vanitha Kunta <vanithakunta2406@gmail.com>
2019-03-06 23:30:07 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,20 +5,14 @@ import {
   StyleSheet,
   Text,
   View,
-<<<<<<< HEAD
-=======
   Button,
   Image,
->>>>>>> 23b0d1058ebf6a1c0964b250aa698fbd02a3a0fc
   TouchableOpacity,
   ImageBackground
 } from 'react-native';
-//import ImageBackground from 'react-native';
  
 import Swiper from 'react-native-swiper';
 
- 
- 
 const styles = StyleSheet.create({
   wrapper: {
   },
@@ -26,28 +20,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-<<<<<<< HEAD
     backgroundColor: '#ffffff',
-
-=======
-    backgroundColor: '#ffff',
->>>>>>> 23b0d1058ebf6a1c0964b250aa698fbd02a3a0fc
   },
   slide2: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-<<<<<<< HEAD
     backgroundColor: '#ffffff',
-=======
-    backgroundColor: '#ffff',
->>>>>>> 23b0d1058ebf6a1c0964b250aa698fbd02a3a0fc
   },
   slide3: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-<<<<<<< HEAD
     backgroundColor: '#ffffff',
   },
   text: {
@@ -64,10 +48,7 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     flex: 1,
-    resizeMode: 'stretch', // or 'stretch',
-    //width:500,
-=======
-    backgroundColor: '#ffff',
+    resizeMode: 'stretch', 
   },
   text: {
     color: '#ffff',
@@ -77,7 +58,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     textAlign: 'center',
     backgroundColor: '#202020',
->>>>>>> 23b0d1058ebf6a1c0964b250aa698fbd02a3a0fc
   },
 })
  
@@ -93,13 +73,8 @@ export default class Swipers extends Component {
   }
   render(){
     return (
-<<<<<<< HEAD
-
       <Swiper style={styles.wrapper} showsButtons={true} index={1} loop={false}>
-        <View style={styles.slide1}>
-<<<<<<< HEAD
-          <Text style={styles.text}>Navigation Mode</Text>
-=======
+      <View style={styles.slide1}>
         <ImageBackground source={require('./bg4.jpg')} style={styles.backgroundImage}>
         <TouchableOpacity
          style={styles.button}
@@ -117,44 +92,19 @@ export default class Swipers extends Component {
           <Text style={styles.text}>Trust Drive</Text>   
           </TouchableOpacity>
           </ImageBackground>
->>>>>>> e2b0c9b2f3a9a970188b22833ee0e25d9e2c2880
         </View>
 
         <View style={styles.slide2}>
-<<<<<<< HEAD
-          <Text style={styles.text}>Mapping Mode</Text>
-=======
         <ImageBackground source={require('./bg4.jpg')} style={{width:500,height:null,flex:1}}>
         <TouchableOpacity
          style={styles.button}
          onPress={this.onPress}>
-          <Text style={styles.text}>Mapping Mode</Text>
+          <Text style={styles.text}>Mapper Mode</Text>
           </TouchableOpacity>
           </ImageBackground>
->>>>>>> e2b0c9b2f3a9a970188b22833ee0e25d9e2c2880
         </View>
-      </Swiper>
-      
+      </Swiper>  
     );
   }
 }
- 
-//AppRegistry.registerComponent('myproject', () => Swiper);
-=======
-      <Swiper style={styles.wrapper} showsButtons={true} index={1} loop={false}>
-        <View style={styles.slide1}>
-          <Text style={styles.text}>NAVIGATION MODE</Text>  
-        </View>
-        
-        <View style={styles.slide3}>
-          <Text style={styles.text}>TRUST DRIVE</Text> 
-        </View>
-
-        <View style={styles.slide2}>   
-          <Text style={styles.text}>MAPPER MODE</Text>   
-        </View>
-      </Swiper> 
-    );
-  }
-}
->>>>>>> 23b0d1058ebf6a1c0964b250aa698fbd02a3a0fc
+     
